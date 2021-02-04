b43f82c1b21bcc6ff998a7f667754f44fb385164
ZachHill21 <814055@mystma.org>
2021-02-04 17:35:31 +0000

    try 1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(w, y):
+    y == 0
+    for i in range(0, len(w)):
+        if w[i] == y:
+            y == (y + 1)
+    return y
+    return False
+print (letterPos("rat", "r"))
